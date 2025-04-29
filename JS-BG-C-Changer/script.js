b599fd56a798d30cf84a1249dc711eeb8597f387
Abdullah-maqsood009 <abdullahmaqsood.social@gmail.com>
2025-04-29 10:56:07 +0500

    Add files via upload

---
+++ JS-BG-C-Changer/script.js
@@ -0,0 +1,26 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+buttons.forEach(function(button){
+    console.log(button);
+    
+    button.addEventListener('click' , function(e){
+        console.log(e.target);
+        
+    if(e.target.id === 'grey'){
+        document.body.style.backgroundColor = e.target.id;
+    }
+    if(e.target.id === 'blue'){
+        document.body.style.backgroundColor = e.target.id;
+    }
+    if(e.target.id === 'green'){
+        document.body.style.backgroundColor = e.target.id;
+    }
+    if(e.target.id === 'yellow'){
+        document.body.style.backgroundColor = e.target.id;
+    }
+    if(e.target.id === 'black'){
+        document.body.style.backgroundColor =e.target.id;
+    }
+    });
+});
